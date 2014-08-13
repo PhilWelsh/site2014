cffd4c6f78e7ad5893485c0835b81e22653869f9
Phil Welsh <phizs@hotmail.com>
2014-08-14 00:24:15 +0100

    carousel

--- javascripts/main.js
+++ javascripts/main.js
@@ -223,4 +223,38 @@ $(function () {
         }
     });
 
+    $('.portexamples').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+    {
+        breakpoint: 1500,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+        }
+    },
+    {
+        breakpoint: 1050,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 775,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }
+        ]
+
+});
+
+
+
 });
